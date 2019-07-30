41ff0dae13bc7ccec4ca7f890f64543a97c3fca8
Shaun-X <hanx9607@gmail.com>
2019-07-29 19:48:09 -0700

    updated class exercises

--- helloWorld.java
+++ helloWorld.java
@@ -6,7 +6,32 @@ public class helloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//hello world exercise
+		System.out.println("hello world!");
+		//exercise 1
+		exercise_1();
+		// convert 100lb to kg
+		lbTokg();
+		// switch coding exercise
+		switch_exercise();
+			
+		}
+	public static void exercise_1(){
+		int i = 500;
+		short s = 10;
+		byte b = 30;
+		long l = 70000 + 15 * i + s + b;
+		System.out.println(l);
+	}
+	public static void lbTokg(){
+		int lb = 100;
+		double kg = 0.45359237 * lb;
+		System.out.println("100lb is " + kg + "kg");
+	}
+	public static void switch_exercise(){
 		char c = ' ';
+		String s = " ";
+		do{
 		System.out.println("Please enter a letter: ");
 		Scanner scnr = new Scanner(System.in);
 		c = scnr.next().charAt(0);;
@@ -29,9 +54,16 @@ public class helloWorld {
 			default: 
 				System.out.println("No such fruit");
 				break;
-			
+			}
+			System.out.println("Do you wish to continue?: ");
+			System.out.println("Yes or No");
+			s = scnr.next();
+		}while(s.equalsIgnoreCase("yes"));
+		if(s.equalsIgnoreCase("no")){
+			System.out.println("See you next time!");
 		}
+	}
 		
 	}
 
-}
+
